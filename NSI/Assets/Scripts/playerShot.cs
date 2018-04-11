ad6df5c5b6bcff1ff7dfc48dbdb68ec25f7d4280
Sarahjane Cregan <C16703349@mydit.ie>
2018-04-11 14:41:36 +0100

    created player bullet and added the player shot script to it

--- NSI/Assets/Scripts/playerShot.cs
+++ NSI/Assets/Scripts/playerShot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerShot : MonoBehaviour {
     public int speed = 3;
     public int damage;
     
